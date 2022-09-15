e46466b6537f573bb850fba699ec3aebf401dd81
leeyounggyu <baayoo93@naver.com>
2022-09-15 20:35:47 +0900

    Feat: props로 내려주는 ref 를 forwardRef 방식으로 변경

--- src/components/EditorUi.tsx
+++ src/components/EditorUi.tsx
@@ -4,25 +4,23 @@ import "tui-color-picker/dist/tui-color-picker.css";
 import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
+import React, { forwardRef } from "react";
 
-interface EditorUiProps extends EditorProps {
+interface EditorWithForwardProps extends EditorProps {
   theme: string;
   height: string;
   onChange: (x: string) => void;
   editorRef: React.MutableRefObject<null>;
 }
 
-const EditorUi = ({ theme, height, onChange, editorRef }: EditorUiProps) => {
-  return (
-    <>
-      <Editor
-        height={height}
-        theme={theme}
-        plugins={[colorSyntax]}
-        ref={editorRef}
-        onChange={onChange}
-      />
-    </>
-  );
-};
+const EditorUi = forwardRef((props: EditorWithForwardProps) => (
+  <Editor
+    height={props.height}
+    theme={props.theme}
+    plugins={[colorSyntax]}
+    ref={props.editorRef}
+    onChange={props.onChange}
+  />
+));
+
 export default EditorUi;
--- src/pages/wirte.tsx
+++ src/pages/wirte.tsx
@@ -1,18 +1,17 @@
 import { useRef } from "react";
 import type { NextPage } from "next";
-import { Button, Editor } from "../components";
+import { Editor } from "../components";
 
 const Write: NextPage = () => {
   const editorRef = useRef(null);
-
   const onChange = () => {
     const data = editorRef.current.getInstance().getMarkdown();
     console.log(data);
+    // return data;
   };
 
   return (
     <>
-      <Button />
       <Editor
         height="600px"
         theme="dark"
