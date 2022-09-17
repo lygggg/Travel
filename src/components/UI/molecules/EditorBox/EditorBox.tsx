14b01bf7d7b5a14a777ecf9c4e7d9c6c2276f7c8
leeyounggyu <baayoo93@naver.com>
2022-09-17 23:14:35 +0900

    Refactor: EditorBox를 molecules로 이동

--- src/components/UI/molecules/EditorBox/EditorBox.tsx
+++ src/components/UI/molecules/EditorBox/EditorBox.tsx
@@ -4,19 +4,19 @@ import "tui-color-picker/dist/tui-color-picker.css";
 import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
-import React, { useRef } from "react";
+import React, { useRef, Dispatch, SetStateAction } from "react";
 
 interface EditorUiProps extends EditorProps {
   theme: string;
   height: string;
-  getEditorContent: (x: string) => void;
+  onChange: Dispatch<SetStateAction<string>>;
 }
-const EditorBox = ({ height, theme, getEditorContent }: EditorUiProps) => {
+const EditorBox = ({ height, theme, onChange }: EditorUiProps) => {
   const editorRef = useRef<Editor>(null);
   const onChangeText = () => {
     if (!editorRef.current) return;
     const data = editorRef.current.getInstance().getMarkdown();
-    getEditorContent(data);
+    onChange(data);
   };
 
   return (
