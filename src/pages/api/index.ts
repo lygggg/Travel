190dfdd19dfccee84cd4a120a6359324e8de5e7e
leeyounggyu <baayoo93@naver.com>
2022-09-21 23:37:39 +0900

    Feat: 몽고디비 연결 및 라우터 생성

---
+++ src/pages/api/index.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createRouter, expressWrapper } from "next-connect";
+import connectMongo from "./utils/connectMongo.js";
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+router.use(async (req, _, next) => {
+  await connectMongo();
+  await next();
+});
+export default router;
