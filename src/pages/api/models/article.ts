4d1b2e37de4f189b5378374611cca27a2cac0195
leeyounggyu <baayoo93@naver.com>
2022-09-21 23:38:00 +0900

    Add: 아티클 스키마 생성

---
+++ src/pages/api/models/article.ts
@@ -0,0 +1,11 @@
+import { Schema, model, models } from "mongoose";
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  tag: { type: String, required: true },
+  content: { type: String, required: true },
+  syncTime: { type: Date, default: Date.now },
+});
+const Article = models.Article || model("Article", articleSchema);
+
+export default Article;
