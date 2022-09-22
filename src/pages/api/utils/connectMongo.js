3f1cefaf5a13d8802df068e750a26671f9ed9a15
leeyounggyu <baayoo93@naver.com>
2022-09-22 19:30:36 +0900

    Feat: 몽고디비 연결 한번만 실행하도록 설정

--- src/pages/api/utils/connectMongo.js
+++ src/pages/api/utils/connectMongo.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
-
+const MONGO_URI = process.env.MONGO_URI;
+const connectMongo = async () => {
+  if (!global.mongoose) {
+    global.mongoose = mongoose.connect(MONGO_URI);
+    global.mongoose();
+  }
+};
 export default connectMongo;
