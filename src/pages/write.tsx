b899819c50e0e11c78979d2e834cbfc6186a7978
leeyounggyu <baayoo93@naver.com>
2022-09-28 17:40:10 +0900

    Feat: 로그인시에만 write 페이지 접속 가능하도록 구현

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import { useSession, signIn } from "next-auth/react";
 import { Editor } from "../components/templates";
 
 const Write: NextPage = () => {
+  const { data: session } = useSession();
+  const isUser = !!session?.user;
+  if (!isUser) {
+    signIn();
+  }
   return (
     <>
       <Editor />
