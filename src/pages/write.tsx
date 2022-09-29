18401b0cce16911fed6c12e79d680ce3d662b54f
leeyounggyu <baayoo93@naver.com>
2022-09-30 02:51:44 +0900

    Update write.tsx

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from "next";
-import { useSession, signIn } from "next-auth/react";
 import { Editor } from "../components/templates";
 
 const Write: NextPage = () => {
-  const { data: session } = useSession();
-  const isUser = !!session?.user;
-  if (!isUser) {
-    signIn();
-  }
   return (
     <>
       <Editor />
