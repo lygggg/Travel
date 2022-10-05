eb6551270df13522dad946b0499bdcd105bd5c8e
leeyounggyu <baayoo93@naver.com>
2022-10-05 16:41:11 +0900

    Feat: 폰트 색상 설정 기능 삭제

--- src/components/editor/EditorBox/EditorBox.tsx
+++ src/components/editor/EditorBox/EditorBox.tsx
@@ -1,7 +1,4 @@
 import { Editor, EditorProps } from "@toast-ui/react-editor";
-import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-import "tui-color-picker/dist/tui-color-picker.css";
-import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 import React, { useRef, Dispatch, SetStateAction } from "react";
@@ -39,7 +36,6 @@ const EditorBox = ({ height, theme, onChange }: EditorUiProps) => {
       <Editor
         height={height}
         theme={theme}
-        plugins={[colorSyntax]}
         onChange={onChangeText}
         hooks={{
           addImageBlobHook: async (blob, callback) =>
