f54cb22c779733081f1be995b1d0c09a9d095f7d
leeyounggyu <baayoo93@naver.com>
2022-10-22 17:10:46 +0900

    Feat: global theme color 추가

---
+++ src/models/emotion.d.ts
@@ -0,0 +1,29 @@
+import "@emotion/styled";
+
+interface Palette {
+  50?: Color;
+  100?: Color;
+  200?: Color;
+  300?: Color;
+  400?: Color;
+  500?: Color;
+  600?: Color;
+  700?: Color;
+  800?: Color;
+  900?: Color;
+}
+
+declare module "@emotion/react" {
+  export interface Theme {
+    primary: Palette;
+    red: Palette;
+    orange: Palette;
+    yellow: Palette;
+    green: Palette;
+    blue: Palette;
+    purple: Palette;
+    black: Palette;
+    gray: Palette;
+    white: Color;
+  }
+}
