77d6b5098d1557bcf3d0420147f0d89b9456b326
leeyounggyu <baayoo93@naver.com>
2022-10-22 21:54:44 +0900

    Add: TextArea component 추가

---
+++ src/components/commons/TextArea/TextArea.tsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react";
+import styled from "@emotion/styled";
+
+interface TextAreaStyled
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  onChange: any; // TODO any
+  label: string;
+}
+
+const TextArea = (props: TextAreaStyled) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const [textLength, setTextLength] = useState(0);
+
+  const handleChange = () => {
+    const text = ref.current?.value;
+    setTextLength(text?.length ?? 0);
+    props.onChange?.(text);
+  };
+
+  return (
+    <>
+      <Label>
+        <LabelText>{props.label && `${props.label}`}</LabelText>
+        <TextAreaStyled
+          {...props}
+          ref={ref}
+          value={props.value}
+          onChange={handleChange}
+        />
+        <TextLength>
+          {props.maxLength && `${textLength}/${props.maxLength}`}
+        </TextLength>
+      </Label>
+    </>
+  );
+};
+export default TextArea;
+
+const TextAreaStyled = styled.textarea<TextAreaStyled>`
+  width: 100%;
+  padding: 0.75rem;
+  font-size: 1rem;
+  background-color: ${(props) => props.theme.black[500]};
+  color: ${(props) => props.theme.white};
+  outline: none;
+  resize: none;
+`;
+
+const TextLength = styled.div`
+  text-align: end;
+`;
+
+const Label = styled.label`
+  flex-direction: column;
+  display: flex;
+  gap: 1rem;
+`;
+
+const LabelText = styled.span`
+  font-size: 1.5rem;
+`;
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -2,3 +2,4 @@ export * from "./Button";
 export * from "./Input";
 export * from "./Modal";
 export * from "./IconButton";
+export * from "./TextArea";
