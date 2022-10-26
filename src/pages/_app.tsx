208bb4d7970473e67d08dcf30e3bfd8f2ddad45e
leeyounggyu <baayoo93@naver.com>
2022-10-26 15:41:42 +0900

    Fix: git conflicts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,27 @@ import { HeaderBar } from "src/components/header";
 import { GlobalStyle, theme } from "src/styles/globalStyle";
 import { ThemeProvider } from "@emotion/react";
 import { HeadMeta } from "src/components/commons";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "src/libs/gtag";
+
 function MyApp({
   Component,
   pageProps,
 }: AppProps<{
   session: Session;
 }>) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <SessionProvider session={pageProps.session}>
       <HeadMeta />
