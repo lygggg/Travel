ae4953b38f67a43446315162d06ed77c0bdd0089
leeyounggyu <baayoo93@naver.com>
2022-10-27 18:23:24 +0900

    Feat: serverside sitemap 설정

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,36 @@
+import { GetServerSidePropsContext } from "next";
+import { getServerSideSitemap } from "next-sitemap";
+import ArticleModel from "src/pages/api/models/article";
+import connectMongo from "src/pages/api/utils/connectMongo";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  await connectMongo();
+  const articles = await ArticleModel.find();
+  const lastmod = new Date().toISOString();
+
+  const AbsoluteFields = [
+    {
+      loc: `${process.env.NEXT_PUBLIC_URL}`, // Absolute url        changefreq: 'daily',
+      changefreq: "daily",
+      lastmod,
+      priority: 0.7,
+    },
+  ];
+
+  // TODO 카테고리 sitemap 추가해야함
+
+  const articleFields = articles.map((article) => ({
+    loc: `${process.env.NEXT_PUBLIC_URL}/articles/${article._id}`,
+    priority: 1.0,
+    lastmod,
+  }));
+
+  const fields = [...AbsoluteFields, ...articleFields];
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+// Default export to prevent next.js errors
+export default () => {
+  return;
+};
