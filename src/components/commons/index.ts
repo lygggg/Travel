fa582c50c879d59524b10cfbf51a387b05ba2496
leeyounggyu <baayoo93@naver.com>
2022-10-30 18:27:59 +0900

    Refactor: taglist component 생성

--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -3,5 +3,5 @@ export * from "./Input";
 export * from "./Modal";
 export * from "./IconButton";
 export * from "./TextArea";
-export * from "./Tag";
+export * from "./TagList";
 export * from "./HeadMeta";
