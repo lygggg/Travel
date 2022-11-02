ad610e9941c3590bffe4189c311c66c4eeb58c59
leeyounggyu <baayoo93@naver.com>
2022-11-03 02:15:42 +0900

    Rename: article에서 articles로 변경

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,13 +1,24 @@
 import api from "./core";
-import { findArticleRequest, PostArticleRequest } from "src/models/article";
+import {
+  findArticlesRequest,
+  findArticleRequest,
+  PostArticleRequest,
+} from "src/models/article";
 
 export const postArticle = async (data: PostArticleRequest) => {
   await api.post("/api/article", data);
 };
 
-export const findArticles = async ({ userId, tag }: findArticleRequest) => {
+export const findArticles = async ({ userId, tag }: findArticlesRequest) => {
   const { data } = await api.get(
     encodeURI(`/api/users/${userId as string}/articles?q=${tag as string}`),
+  );
+  return data;
+};
+
+export const findArticle = async ({ userId, id }: findArticleRequest) => {
+  const { data } = await api.get(
+    encodeURI(`/api/users/${userId as string}/articles/${id as string}`),
   );
   return data;
 };
