4b18dbccc90231ca81fc655edb3a5e10abd45389
leeyounggyu <baayoo93@naver.com>
2022-11-12 04:35:08 +0900

    Feat: add articleTitle component

--- src/components/article/ArticleTitle/ArticleTitle.tsx
+++ src/components/article/ArticleTitle/ArticleTitle.tsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
+import { useMutation } from "@tanstack/react-query";
 import Image from "next/image";
+import Link from "next/link";
 import { TagList } from "src/components/commons";
 
 interface Props {
@@ -8,12 +10,21 @@ interface Props {
   img: string;
   base64: string;
   syncTime: string;
+  _id: string;
 }
 const ArticleTitle: React.FC<Props> = (article) => {
-  const { title, tags, img, base64, syncTime } = article;
+  const { title, tags, img, base64, syncTime, _id } = article;
+
+  const removeArticle = useMutation;
   return (
     <Container>
       <Title>{title}</Title>
+      <div>
+        <Link href={{ pathname: "/write", query: { id: _id } }}>
+          <span>수정</span>
+        </Link>
+        <span>삭제</span>
+      </div>
       {syncTime}
       <ImageContainer>
         <Image
