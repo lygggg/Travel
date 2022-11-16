861e6550ca6d54e475941c3a0269c45b4fbcb779
leeyounggyu <baayoo93@naver.com>
2022-11-16 23:53:37 +0900

    Feat: headerbar 스타일 수정

--- src/components/header/HeaderBar/HeaderBar.tsx
+++ src/components/header/HeaderBar/HeaderBar.tsx
@@ -1,42 +1,55 @@
 import styled from "@emotion/styled";
 import { useSession, signIn } from "next-auth/react";
+import Link from "next/link";
 import { Button, IconButton } from "src/components/commons";
+import { LoginModal } from "src/components/login/LoginModal";
+import useModal from "src/hooks/useModal";
 import { MenuDropdown } from "../index";
 
 const HeaderBar = () => {
   const { data: session } = useSession();
+  const [openModal, closeModal, modalOpen] = useModal();
+
+  const handleModalOpen = () => openModal();
 
   return (
-    <HeaderContainer>
-      <HeaderLayout>
-        <HeaderLeftContainer>
-          <Span>HOME</Span>
-          {session && <Span>{session.user.name}</Span>}
-        </HeaderLeftContainer>
-        <HeaderRightContainer>
-          <IconButton width="30px" height="30px" position="-110px -10px" />
-          <IconButton width="30px" height="30px" position="-10px -10px" />
+    <>
+      <LoginModal isActive={modalOpen} handleClose={closeModal} />
+      <HeaderContainer>
+        <HeaderLayout>
+          <HeaderLeftContainer>
+            <StyledLink href={"/"}>Mlog</StyledLink>
+            {session && (
+              <StyledLink href={`/${session.user.email}`}>
+                {session.user.name + ".log"}
+              </StyledLink>
+            )}
+          </HeaderLeftContainer>
+          <HeaderRightContainer>
+            <IconButton width="30px" height="30px" position="-110px -10px" />
+            <IconButton width="30px" height="30px" position="-10px -10px" />
 
-          {session ? (
-            <MenuDropdown />
-          ) : (
-            <Button
-              variant="primary"
-              size="large"
-              rounded="round"
-              onClick={() => signIn()}
-            >
-              로그인
-            </Button>
-          )}
-        </HeaderRightContainer>
-      </HeaderLayout>
-    </HeaderContainer>
+            {session ? (
+              <MenuDropdown />
+            ) : (
+              <Button
+                variant="primary"
+                size="medium"
+                rounded="round"
+                onClick={handleModalOpen}
+              >
+                로그인
+              </Button>
+            )}
+          </HeaderRightContainer>
+        </HeaderLayout>
+      </HeaderContainer>
+    </>
   );
 };
 
 const HeaderContainer = styled.header`
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(100, 100, 100, 0.5);
   top: 0;
   left: 0;
   right: 0;
@@ -62,8 +75,10 @@ const HeaderRightContainer = styled.div`
   align-items: center;
   gap: 20px;
 `;
-const Span = styled.span`
+const StyledLink = styled(Link)`
   color: white;
+  cursor: pointer;
+  font-size: 1.3rem;
 `;
 
 const HeaderLeftContainer = styled.div`
