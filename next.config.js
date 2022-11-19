37e4edbec63f739d4f03a44da08b72932cbe5182
leeyounggyu <baayoo93@naver.com>
2022-11-19 11:26:45 +0900

    Chore: next.config withSentry 수정

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
     domains: ["mlog-lygggg.s3.ap-northeast-2.amazonaws.com"],
     formats: ["image/webp"],
   },
+  sentry: {
+    // disableServerWebpackPlugin: true, 서버, 클라이언트 별도로 처리하는 경우 플러그인 비활성화 가능함
+    // disableClientWebpackPlugin: true,
+    // autoInstrumentServerFunctions: false, 오류및 성능 모니터링, api 자동계측 설정
+    hideSourceMaps: true,
+  },
 };
 
 // safely ignore recoil stdout warning messages
@@ -26,19 +32,6 @@ const interceptStdout = (text) => {
 // Intercept in dev and prod
 intercept(interceptStdout);
 
-const moduleExports = {
-  // Your existing module.exports
-
-  sentry: {
-    // disableServerWebpackPlugin: true, 서버, 클라이언트 별도로 처리하는 경우 플러그인 비활성화 가능함
-    // disableClientWebpackPlugin: true,
-    // autoInstrumentServerFunctions: false, 오류및 성능 모니터링, api 자동계측 설정
-    hideSourceMaps: true,
-  },
-  // productionBrowserSourceMaps: true,
-  // 소스맵 생성
-};
-
 const sentryWebpackPluginOptions = {
   silent: true,
 };
@@ -47,7 +40,7 @@ module.exports = (_phase, { defaultConfig }) => {
   const plugins = [
     withPlaiceholder,
     withBundleAnalyzer,
-    () => withSentryConfig(moduleExports, sentryWebpackPluginOptions),
+    (nextConfig) => withSentryConfig(nextConfig, sentryWebpackPluginOptions),
   ];
   return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
 };
