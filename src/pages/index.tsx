95f6b0cc5ecf5f13b9874725f45b2159a2b38383
leeyounggyu <baayoo93@naver.com>
2022-11-21 15:33:23 +0900

    Feat: main page 로그인, 글쓰기 버튼 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { Button } from "src/components/commons";
 import { TextBox } from "src/components/home";
 
-const WELCOME = "나만의 블로그를 만들어보세요.";
+const WELCOME = "블로그에서 나만의 스토리를 공유해 보세요.";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -23,22 +23,13 @@ const Home: NextPage = () => {
 
   return (
     <Container>
+      <Title>MLOG</Title>
       <TextBox content={WELCOME} speed={100}></TextBox>
       <ButtonContainer>
-        <Button
-          onClick={handleClickWrite}
-          variant="primary"
-          size="large"
-          rounded="round"
-        >
+        <Button onClick={handleClickWrite} variant="primary" size="large">
           글 작성하러 가기
         </Button>
-        <Button
-          onClick={handleClickMe}
-          variant="default"
-          size="large"
-          rounded="round"
-        >
+        <Button onClick={handleClickMe} variant="default" size="large">
           내 블로그가 가기
         </Button>
       </ButtonContainer>
@@ -48,7 +39,7 @@ const Home: NextPage = () => {
 
 const Container = styled.div`
   display: flex;
-  row-gap: 2rem;
+  row-gap: 4rem;
   flex-direction: column;
   place-items: center;
   width: 100%;
@@ -56,9 +47,23 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const Title = styled.h1`
+  font-size: 5rem;
+  @keyframes fadein {
+    0% {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    100% {
+      opacity: 1;
+      transform: none;
+    }
+  }
+`;
+
 const ButtonContainer = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 2rem;
 `;
 
 export default Home;
