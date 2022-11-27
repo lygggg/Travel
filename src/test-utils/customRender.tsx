e3f8495075ef53613fdff3dae01db36388482a13
leeyounggyu <baayoo93@naver.com>
2022-11-28 02:11:46 +0900

    Test: add customRender

---
+++ src/test-utils/customRender.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement, ReactNode } from "react";
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "@emotion/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+
+import { createMockRouter } from "__mocks__/createMockRouter";
+import { theme } from "src/styles/globalStyle";
+import { queryClient } from "src/test-utils/ReactQueryWrapper";
+
+interface Props {
+  children: ReactNode;
+}
+
+const defaultQueryClient = queryClient;
+const defaultTheme = theme;
+const defaultRouter = createMockRouter({
+  query: { userId: "baayoo93@gmail.com", id: "idfsdfds" },
+  push: jest.fn(),
+});
+
+const customRender = (
+  ui: ReactElement,
+  {
+    theme = defaultTheme,
+    router = defaultRouter,
+    queryClient = defaultQueryClient,
+    ...renderOptions
+  },
+) => {
+  function Wrapper({ children }: Props) {
+    return (
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <RouterContext.Provider value={router}>
+              {children}
+            </RouterContext.Provider>
+          </QueryClientProvider>
+        </RecoilRoot>
+      </ThemeProvider>
+    );
+  }
+  return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
+
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
