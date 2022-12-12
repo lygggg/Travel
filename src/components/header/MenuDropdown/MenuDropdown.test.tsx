1acdfaddd59358ee5f08c3010acc20acf1534b92
leeyounggyu <baayoo93@naver.com>
2022-12-12 10:48:50 +0900

    Refactor: MenuDropdown test 리팩터링

--- src/components/header/MenuDropdown/MenuDropdown.test.tsx
+++ src/components/header/MenuDropdown/MenuDropdown.test.tsx
@@ -1,11 +1,7 @@
-import { fireEvent, waitFor } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { signOut, useSession } from "next-auth/react";
 import { render } from "src/test-utils/customRender";
 import MenuDropdown, { Props } from "./MenuDropdown";
-import {
-  DropDownContext,
-  DropDownContent,
-} from "src/components/commons/DropDown/DropDown";
 
 jest.mock("next-auth/react");
 const mockUseSession = (data: { email: string }) => {
@@ -19,13 +15,8 @@ const mockUseSession = (data: { email: string }) => {
 };
 
 describe("MenuDropdown", () => {
-  const renderMenuDropdown = (props: Props & { value: DropDownContent }) =>
-    render(
-      <DropDownContext.Provider value={props.value}>
-        <MenuDropdown {...props}></MenuDropdown>
-      </DropDownContext.Provider>,
-      {},
-    );
+  const renderMenuDropdown = (props: Props) =>
+    render(<MenuDropdown {...props}></MenuDropdown>, {});
 
   const menus = [
     { title: "새 글 작성", url: "/write" },
@@ -39,16 +30,28 @@ describe("MenuDropdown", () => {
   context("dropdown이 열려있을 때", () => {
     context("로그아웃 버튼 누르면", () => {
       it("로그아웃 메서드가 호출된다.", async () => {
-        const { getByText } = renderMenuDropdown({
+        renderMenuDropdown({
           items: menus,
           trigger: <button>버튼</button>,
-          value: { isActive: true },
         });
 
-        const button = getByText("로그아웃");
+        const button = screen.getByText("로그아웃");
         fireEvent.click(button);
 
         expect(signOut).toBeCalled();
+      });
+    });
+
+    context("드랍다운 메뉴 아이템을 누르면", () => {
+      it("해당 url로 이동한다.", () => {
+        renderMenuDropdown({
+          items: menus,
+          trigger: <button>버튼</button>,
+        });
+
+        const link = screen.getByText(`${menus[0].title}`).closest("a");
+
+        expect(link).toHaveAttribute("href", `${menus[0].url}`);
       });
     });
   });
