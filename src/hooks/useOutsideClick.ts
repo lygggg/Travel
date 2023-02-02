30e2e8c0b6c99cb3de01f54c8c505ace17b11f8c
leeyounggyu <baayoo93@naver.com>
2023-02-02 15:29:35 +0900

    Feat: useOutsideClick hook 추가

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,18 @@
+import { useEffect, RefObject } from "react";
+
+const useOutsideClick = (ref: RefObject<HTMLElement>, closeModal: any) => {
+  useEffect(() => {
+    const onClick = (e: Event) => {
+      if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("mousedown", onClick);
+    return () => {
+      document.removeEventListener("mousedown", onClick);
+    };
+  }, [ref]);
+};
+
+export default useOutsideClick;
